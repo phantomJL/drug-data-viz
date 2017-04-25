164549a55c38b155656dcec9dd370994cb12e56f
Ty Roderick <tyroderick0614@gmail.com>
2017-04-24 17:55:45 -0700

    Added all filters
    Still need to add csv files

--- visualization.js
+++ visualization.js
@@ -12,24 +12,53 @@ var chart = c3.generate({
 
 function changeData(dataToLoad){
 	chart.unload();
-	setTimeout(function () {
-		switch(dataToLoad) {
-		    case 'total-deaths':
-		        chart.load({
-		        	url: 'data/total-deaths.csv'
-		    	});
-		        break;
-		    case 'opioid-type':
-		        chart.load({
-		        	url: 'data/opioid-type.csv'
-		    	});
-		        break;
-		    default:
-		        chart.load({
-		        	url: 'data/total-deaths.csv'
-		    	});
-		} 
-		
-	}, 1000);
+	switch(dataToLoad) {
+	    case 'total-deaths':
+	        chart.load({
+	        	url: 'data/total-deaths.csv'
+	    	});
+	    	$("#filter-dropdown").html('All Deaths <span class="caret"></span>');
+	        break;
+	    case 'age':
+	     //    chart.load({
+	     //    	url: 'data/opioid-type.csv'
+	    	// });
+	    	$("#filter-dropdown").html('Age <span class="caret"></span>');
+	        break;
+	    case 'county':
+	     //    chart.load({
+	     //    	url: 'data/total-deaths.csv'
+	    	// });
+	    	$("#filter-dropdown").html('County <span class="caret"></span>');
+	        break;
+	    case 'ethnicity':
+	     //    chart.load({
+	     //    	url: 'data/opioid-type.csv'
+	    	// });
+	    	$("#filter-dropdown").html('Ethnicity <span class="caret"></span>');
+	        break;
+	    case 'expected-source':
+	     //    chart.load({
+	     //    	url: 'data/total-deaths.csv'
+	    	// });
+	    	$("#filter-dropdown").html('Expected Source of Payment <span class="caret"></span>');
+	        break;
+	    case 'opioid-type':
+	        chart.load({
+	        	url: 'data/opioid-type.csv'
+	    	});
+	    	$("#filter-dropdown").html('Opioid Type <span class="caret"></span>');
+	        break;
+	    case 'sex':
+	     //    chart.load({
+	     //    	url: 'data/total-deaths.csv'
+	    	// });
+	    	$("#filter-dropdown").html('Sex <span class="caret"></span>');
+	        break;
+	    default:
+	        chart.load({
+	        	url: 'data/total-deaths.csv'
+	    	});
+	} 
 
 }
