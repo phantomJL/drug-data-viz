94ca8388c272521e8a0f185a1b65f5f0c4429034
Ty Roderick <tyroderick0614@gmail.com>
2017-04-24 17:43:36 -0700

    Add filters for 2 data types

--- visualization.js
+++ visualization.js
@@ -1,5 +1,35 @@
 var chart = c3.generate({
     data: {
-        url: 'data/opioid-type.csv'
+        x: 'x',
+        url: 'data/total-deaths.csv',
+        axis: {
+	        x: {
+	            type: 'category' // this needed to load string x value
+	        }
+    	},
     }
 });
+
+function changeData(dataToLoad){
+	chart.unload();
+	setTimeout(function () {
+		switch(dataToLoad) {
+		    case 'total-deaths':
+		        chart.load({
+		        	url: 'data/total-deaths.csv'
+		    	});
+		        break;
+		    case 'opioid-type':
+		        chart.load({
+		        	url: 'data/opioid-type.csv'
+		    	});
+		        break;
+		    default:
+		        chart.load({
+		        	url: 'data/total-deaths.csv'
+		    	});
+		} 
+		
+	}, 1000);
+
+}
